d45182e653a497d5da5fd617e79e0ac591fc6909
Max Batchelder <max@first.ac>
2022-08-05 13:24:10 -0400

    Going to have to restructure to better accomodate support combinations

--- HeightCalc/HeightCalc/ViewController.swift
+++ HeightCalc/HeightCalc/ViewController.swift
@@ -16,48 +16,43 @@ class ViewController: UIViewController {
         Item(name: "half-apple", height: 4),
         Item(name: "quarter-apple", height: 2),
         Item(name: "pancake", height: 1)
-    ]
+        ]
     
     var supportitems: Array = [
         Item(name: "", height: 0),
-        Item(name:"rolling spreaders", height: 4, combineswith: ["babies", "standards"], accessory: true, canuseboxes: false),
         Item(name:"babies", minheight:20, maxheight:36, combineswith: ["rolling spreaders"]),
         Item(name:"standards", minheight:36, maxheight:66, combineswith: ["rolling spreaders"]),
         Item(name:"hi-hat", height: 6),
         Item(name:"lo-hat", height: 3),
-        Item(name:"Fischer 11 standard head", minheight: 14, maxheight: 51, canuseboxes: false),
-        Item(name:"Fischer 11 low head", minheight: 3, maxheight:39, canuseboxes: false)
-    ]
+        Item(name:"Fischer 11 standard head", minheight: 14, maxheight: 51, combineswith: ["18-inch riser", "6-inch riser"], canuseboxes: false),
+        Item(name:"Fischer 11 low head", minheight: 3, maxheight:39, combineswith: ["6-inch riser", "18-inch riser"], canuseboxes: false)
+        ]
     
-    var aksitems: Array = [
+    var supportaks: Array = [
+        Item(name:"18-inch riser", height: 18),
+        Item(name:"6-inch riser", height: 6),
+        Item(name:"rolling spreaders", height: 4, canuseboxes: false),
+        ]
+        
+    var cameraaks: Array = [
         Item(name:"none"),
         Item(name:"stormtrooper slider", height:6, heightonboxes: 4),
         Item(name:"8-ball slider", height:6, heightonboxes: 4),
-        Item(name:"tango", height:2, canuseboxes:false)
-    ]
+        Item(name:"tango", height:2, canuseboxes:false),
+        Item(name:"R-O", height:5, canuseboxes: false),
+        Item(name:"rolling spreaders", height: 4, canuseboxes: false)
+        ]
     
     var heads: Array = [
         Item(name:"2575", height: 16),
         Item(name:"Arrihead", height: 22)
-    ]
-    
-/*
-    // sticks heights are floor to mitchell on flat spreaders
-    let babies = [20, 36]
-    let standards = [36, 66]
-    let rollers = 4
-    let mitchelltolens = 16
-    let lohat = 3
-    let hihat = 6
-    
-    let sliderfrommitchell = 6
-    let sliderfrombase = 4
-    var usingslider = false*/
+        ]
     
     var goalheight = 0
     var outputtext = ""
     var currenthead = "2575"
     var currentconfig : Array<String> = []
+    var preferrollers = true
     
     @IBOutlet weak var outputLabel: UILabel!
     @IBOutlet weak var inputField: UITextField!
@@ -95,7 +90,7 @@ class ViewController: UIViewController {
 
     var aksmenuitems: [UIAction] {
         var output : Array<UIAction> = []
-        for aksitem in aksitems {
+        for aksitem in cameraaks {
             output.append(UIAction(title: aksitem.name, handler: {
                 (action) in
                 self.currentconfig.removeAll() //TODO manage more than one
@@ -122,6 +117,10 @@ class ViewController: UIViewController {
         for box in appleboxes {
             let numberof: Int = remainingheight / box.height
             if (numberof > 0) {
+                if ((box.name == "full apple #2" || box.name == "full apple #3") && numberof > 1) {
+                    // No more than 1x of full apple #2 or #3 is allowed
+                    return "abort"
+                }
                 remainingheight -= numberof * box.height
                 var comma = ""
                 if (!firstentry) {
@@ -164,16 +163,13 @@ class ViewController: UIViewController {
         
         for supportitem in supportitems {
             
-            aksheight = 0
+            print("\nChecking support: " + supportitem.name)
             
-            if (supportitem.accessory) {
-                // only use accessories in conjunction with other support items
-                continue
-                
-            }
+            aksheight = 0
             
             if (supportitem.name == "" && currentconfig.count == 0) {
                 // only use the null (boxes-only) support option when a slider is in use
+                print("Support is null, and we're not using a slider, pass.")
                 continue
             }
             
@@ -181,16 +177,20 @@ class ViewController: UIViewController {
             
             for i in 0..<currentconfig.count {
                 // configuration of aks is a *different height* depending on the support method, hence we have to recalculate this for every support method. TODO perhaps break support up into mitchell & non-mitchell to only have to calculate this twice?
-                let aksindex = aksitems.firstIndex(where: {$0.name == currentconfig[i]})
+                let aksindex = cameraaks.firstIndex(where: {$0.name == currentconfig[i]})
                 if (aksindex != nil) {
+                    print("using accessory " + currentconfig[i])
                     if (supportitem.name == "") {
-                        if (aksitems[aksindex ?? 0].canuseboxes) {
+                        if (cameraaks[aksindex ?? 0].canuseboxes) {
                             // flag that one of our support items can use boxes alone as support (ie a slider)
                             justboxesokay = true
+                            print("accessory can use boxes")
                         }
-                        aksheight += aksitems[aksindex ?? 0].heightonboxes
+                        aksheight += cameraaks[aksindex ?? 0].heightonboxes
+                        print("calculating from boxes")
                     } else {
-                        aksheight += aksitems[aksindex ?? 0].height
+                        aksheight += cameraaks[aksindex ?? 0].height
+                        print("calculating from mitchell")
                     }
                 } else {
                     print ("current config specifies aks that is not available")
@@ -199,102 +199,98 @@ class ViewController: UIViewController {
             
             if (supportitem.name == "" && !justboxesokay) {
                 // no support items were found that can use only boxes as support, moving on...
+                print("Can't continue, not allowed to work on just boxes")
                 continue
             }
             
             goalsupportheight -= aksheight
-            print ("goalsupportheight after aks is " + String(goalsupportheight))
             
+            outputtext += checkSupport(height: goalsupportheight, support: supportitem);
             
-            if ((goalsupportheight >= supportitem.height && supportitem.minheight == 0) || (goalsupportheight >= supportitem.minheight && (goalsupportheight <= supportitem.maxheight || (goalsupportheight > supportitem.maxheight && supportitem.canuseboxes)))) {
-                
-                var appleboxadditions = " "
-                if (supportitem.canuseboxes && goalsupportheight > supportitem.maxheight) {
-                    // only use boxes when we have exceeded the maximum height of a given support
-                    if (supportitem.name != "") {
-                        appleboxadditions += "+ "
-                    }
-                    appleboxadditions += appleboxCalc(height: goalsupportheight - supportitem.heightonboxes)
-                }
-                outputtext += supportitem.name + appleboxadditions + "\n"
-            }
-            
-            goalsupportheight += aksheight
+            goalsupportheight += aksheight // reset the goal height for the next support item (in case support aks is a different height)
         }
-        
-        /*
-        let goalmitchell = goalheight - mitchelltolens // ground to mitchell mount
-        let goalsliderbase = goalmitchell - sliderfrombase // ground to slider base (for applebox use)
-        let goalslidermitchell = goalmitchell - sliderfrommitchell // ground to slider mitchell (for sticks use)
-        var goal = goalmitchell // assume that we want to calculate from ground to head mitchell (no slider)
-        
-        var stickscutoff = babies[0]; // the minimum height for sticks use, assuming no slider
-        if (usingslider) {
-            stickscutoff = babies[0] + sliderfrommitchell // add height of slider if we are using it
-            if (goalmitchell >= stickscutoff) {
-                goal = goalslidermitchell // calculate from ground to slider mitchell mount
-            } else {
-                goal = goalsliderbase // calculate from ground to slider base
-            }
-        }
-        print("stickscutoff is " + String(stickscutoff))
-        print("goalsliderbase is " + String(goalsliderbase))
-        print("goalslidermitchell is " + String(goalslidermitchell))
-        print("goal is " + String(goal))
-        
-        // at this point, use "goal" as point of calculation
-        
-        // first, figure out if we're within bounds
-        if ((usingslider  && (goalslidermitchell < 0)) || (!usingslider && goalmitchell < lohat)) {
-            // special case, the slider mitchell mount is the limiting factor in low cases - if it's below 0 then it's underground
-            outputtext = "Too low for current configuration"
-        } else if (goal > standards[1] + rollers){
-            outputtext = "Too high for current configuration"
-        } else {
-            print("Height is within bounds")
-            
-            if (usingslider) {
-                outputtext += "slider & \n"
-            }
-            
-            if (goalmitchell >= stickscutoff) {
-                print("Height is high enough to use sticks")
-                if (goal >= babies[0] && goal < babies[0] + rollers) {
-                    outputtext += "babies with flat spreaders"
-                    // TODO calculate rise of sticks
-                } else if (goal <= babies[1] + rollers && goal >= standards[0] && goal < standards[0] + rollers) {
-                    outputtext += "babies with rollers,\nor standards with flat spreaders"
-                } else if (goal <= babies[1] + rollers && goal < standards[0] + rollers) {
-                    outputtext += "babies or standards with rollers"
-                } else if (goal >= babies[0] + rollers && goal <= babies[1] + rollers) {
-                    outputtext += "babies with rollers"
-                } else if (goal >= standards[0] + rollers && goal <= standards[1] + rollers) {
-                    outputtext += "standards with rollers"
-                } else if (goal > standards[1]) {
-                    outputtext += "standards with flat spreaders\n" + appleboxCalc(height: goal-standards[1])
-                } else {
-                    outputtext = "Error - height is within sticks range but doesn't fit a case..."
-                }
-            } else {
-                if (usingslider) {
-                    outputtext += appleboxCalc(height: goalsliderbase)
-                } else {
-                    if (goal >= lohat && goalmitchell < hihat) {
-                        outputtext += "lo-hat"
-                        if (goal - lohat > 0) {
-                            outputtext += "\n" + appleboxCalc(height: goal - lohat)
-                        }
-                    } else if (goal >= hihat) {
-                        outputtext += "hi-hat"
-                        if (goal - hihat > 0) {
-                            outputtext += "\n" + appleboxCalc(height: goal - hihat)
-                        }
-                    }
-                }
-            }
-        }*/
         
         outputLabel.text = outputtext
+    }
+    
+    
+    
+    
+    func checkSupport (height: Int, support: Item, startingindex: Int? = 0) -> String {
+        var output = support.name
+        var appleboxadditions = ""
+        var boxesokay = true
+        
+        /*
+         EITHER:
+         
+         height >= support.height && support.minheight == 0 (height is greater than support height, and support is not variable in height)
+         
+         OR:
+         
+         height >= support.minheight (height is greater than or equal to minimum height on a variable-height support)
+         
+            AND EITHER:
+         
+            height <= support.maxheight (height is less than or equal to maximum height on a variable-height support)
+         
+            OR:
+         
+            height > support.maxheight && support.canuseboxes (height is greater than the support's maximum height, but this support can use boxes to get higher)
+         
+         
+         */
+        
+        if ((height >= support.height && support.minheight == 0) || (height >= support.minheight && (height <= support.maxheight || (height > support.maxheight && support.canuseboxes)))) {
+            
+            print("height is greater than this support's minimum, and either less than its maximum or greater than its maximum but can combine")
+            
+            if (height > support.maxheight) {
+                print("height is greater than this support's maximum")
+                
+               /* TODO - support combinations (risers)
+                if (support.combineswith.count > 0) {
+                    print("support can combine")
+                    for i in (startingindex ?? 0)..<support.combineswith.count {
+                        // run through all possible combinations of accessories for this support item
+                        let combinationindex = supportaks.firstIndex(where: {$0.name == support.combineswith[i]})
+                        if (combinationindex != nil) {
+                            // this accessory exists
+                            let currentsupportaks = supportaks[combinationindex ?? 0]
+                            print("checking combination with " + currentsupportaks.name)
+                            let akscheck = checkSupport(height: height - currentsupportaks.height, support: support, startingindex: (startingindex ?? 0)+1) // check this support again, but with height minus the current accessory
+                            if (akscheck != "") {
+                                if (!currentsupportaks.canuseboxes) {
+                                    boxesokay = false;
+                                }
+                                output += " + " + currentsupportaks.name
+                            }
+                        }
+                    }
+                } */
+                
+                if (support.canuseboxes && boxesokay) {
+                    print ("we are allowed to use boxes, calculating for boxes")
+                    // only use boxes when we have exceeded the maximum height of a given support
+                    if (support.name != "") {
+                        appleboxadditions += " + "
+                    }
+                    let appleboxstring = appleboxCalc(height: height - support.heightonboxes)
+                    if (appleboxstring == "abort") {
+                        // ridiculous number of apple boxes is called for, this support scheme doesn't work
+                        return ""
+                    }
+                    appleboxadditions += appleboxstring
+                }
+            } else {
+                print ("height is less than this support's maximum")
+            }
+            
+            output += appleboxadditions + "\n"
+            return output
+        }
+        
+        return ""
     }
     
 }
@@ -310,18 +306,16 @@ class Item {
     var heightonboxes: Int // sliders have a different height when used with boxes than they do on a mitchell mount
     var minheight: Int
     var maxheight: Int
-    var combineswith: Set<String>
-    var accessory: Bool // ie rolling spreaders, which can only be used with another support item
+    var combineswith: Array<String>
     var canuseboxes: Bool // dollies cannot go on boxes
     
-    init(name: String, height: Int? = 0, heightonboxes: Int? = nil, minheight: Int? = nil, maxheight: Int? = nil, combineswith: Set<String>? = nil, accessory: Bool? = false, canuseboxes: Bool? = true) {
+    init(name: String, height: Int? = 0, heightonboxes: Int? = nil, minheight: Int? = nil, maxheight: Int? = nil, combineswith: Array<String>? = nil, canuseboxes: Bool? = true) {
         self.name = name
         self.height = height ?? 0
         self.minheight = minheight ?? self.height
         self.maxheight = maxheight ?? self.height
         self.heightonboxes = heightonboxes ?? self.maxheight
         self.combineswith = combineswith ?? []
-        self.accessory = accessory ?? false
         self.canuseboxes = canuseboxes ?? true
     }
 }
